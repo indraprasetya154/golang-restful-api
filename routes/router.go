9fd6483d6aaa2fe568b15c3a3a6d43192e11775d
Indra Prasetya <indraprasetya154@gmail.com>
2021-11-28 20:26:44 +0700

    Move route to routes folder

---
+++ routes/router.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/indraprasetya154/golang-restful-api/controller"
+	"github.com/indraprasetya154/golang-restful-api/exception"
+	"github.com/julienschmidt/httprouter"
+)
+
+func NewRouter(categoryController controller.CategoryControllerInterface) *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/categories", categoryController.FindAll)
+	router.GET("/categories/:categoryId", categoryController.FindById)
+	router.POST("/categories", categoryController.Create)
+	router.PUT("/categories/:categoryId", categoryController.Update)
+	router.DELETE("/categories/:categoryId", categoryController.Delete)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	return router
+}
