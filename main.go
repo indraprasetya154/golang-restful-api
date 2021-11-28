6d115a399777403fdf19c33f749610cf01d1b58e
Indra Prasetya <indraprasetya154@gmail.com>
2021-11-28 17:35:47 +0700

    Move router to app router

--- main.go
+++ main.go
@@ -8,12 +8,10 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/indraprasetya154/golang-restful-api/app"
 	"github.com/indraprasetya154/golang-restful-api/controller"
-	"github.com/indraprasetya154/golang-restful-api/exception"
 	"github.com/indraprasetya154/golang-restful-api/helper"
 	"github.com/indraprasetya154/golang-restful-api/middleware"
 	"github.com/indraprasetya154/golang-restful-api/repository"
 	"github.com/indraprasetya154/golang-restful-api/service"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -24,15 +22,7 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/categories", categoryController.FindAll)
-	router.GET("/categories/:categoryId", categoryController.FindById)
-	router.POST("/categories", categoryController.Create)
-	router.PUT("/categories/:categoryId", categoryController.Update)
-	router.DELETE("/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
